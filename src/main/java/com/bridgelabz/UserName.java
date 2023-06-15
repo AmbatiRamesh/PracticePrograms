17cef9540c372db966987821a592a2d07b5d9e4d
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-06-15 12:28:18 +0530

    Added: Basic Java Problems

---
+++ src/main/java/com/bridgelabz/UserName.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class UserName {
+    public static void main(String[] args) {
+        String userName = System.getProperty("user.name");
+        System.out.println("User name: " + userName);
+    }
+}
